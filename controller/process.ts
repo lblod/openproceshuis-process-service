7d13318a2e37768ac97cd17421d634e34b87e4dd
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-10-09 11:45:40 +0200

    refactor: instead of the count return a list of organizations with their type
    Refs: oph-682

--- controller/process.ts
+++ controller/process.ts
@@ -1,19 +1,27 @@
 import { querySudo } from '@lblod/mu-auth-sudo';
 import { sparqlEscapeString } from 'mu';
+import { queryResultToJson } from '../util/json-to-csv';
 
-export async function getProcessUsageCountOverOrganizations(processId: string) {
+export async function getOrganizationsUsingProcess(
+  processId: string,
+): Promise<Array<{ id: string; label: string; type: string }>> {
   const queryResult = await querySudo(`
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX prov: <http://www.w3.org/ns/prov#>
     PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX org: <http://www.w3.org/ns/org#>
 
-    SELECT DISTINCT ?organization
+    SELECT DISTINCT ?id ?label ?type
     WHERE {
-      ?process mu:uuid ${sparqlEscapeString(processId)}.
+      VALUES ?id { ${sparqlEscapeString(processId)} }
+      ?process mu:uuid ?id.
       ?process dct:publisher ?self .
       ?process prov:usedBy ?organization .
+      ?organization skos:prefLabel ?label .
+      ?organization org:classification / skos:prefLabel ?type .
     }
   `);
 
-  return queryResult.results?.bindings?.length ?? 0;
+  return queryResultToJson(queryResult);
 }
--- router/process.ts
+++ router/process.ts
@@ -1,17 +1,25 @@
 import Router from 'express-promise-router';
 
 import { Request, Response } from 'express';
-import { getProcessUsageCountOverOrganizations } from '../controller/process';
+import { getOrganizationsUsingProcess } from '../controller/process';
 
 export const processRouter = Router();
 
 processRouter.get(
-  '/:id/count-of-users',
+  '/:id/organizational-usage',
   async (req: Request, res: Response) => {
     try {
-      const count = await getProcessUsageCountOverOrganizations(req.params.id);
+      const usage = await getOrganizationsUsingProcess(req.params.id);
 
-      return res.status(200).send({ count });
+      return res.status(200).send(
+        usage.map((usage) => {
+          return {
+            organizationId: usage.id,
+            label: usage.label,
+            type: usage.type,
+          };
+        }),
+      );
     } catch (error) {
       const message =
         error.message ??
